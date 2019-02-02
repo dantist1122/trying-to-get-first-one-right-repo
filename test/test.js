b7024acdca572055927bcd10a7502f58fad1bd9e
Ivan Cerba <cerba.ivan@gmail.com>
2019-02-03 00:44:51 +0200

    changed test.js

--- test/test.js
+++ test/test.js
@@ -15,10 +15,10 @@ describe('Page opening', function () {
     });
 
     it('reg Button', function(){
-        //browser.click('#registration');
-        let myButton = $('.mt-0');
-        myButton.click();
-        let regButton = browser.waitForEnabled('#fname', 500);
+        browser.click('#registration');
+        // let myButton = $('.mt-0');
+        // myButton.click();
+        let regButton = browser.waitForEnabled('#fname', 10000);
         assert.equal(regButton, true, 'Registration button is not working'); //compare that "title" variable equals to "Bug Tracker" and error-message if not
     });
 });
